c345d12b9771da14fab60447653d9e68912d7001
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-12-12 19:25:34 +0900

    문자열 출력

--- IO/src/boj_11720.java
+++ IO/src/boj_11720.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class boj_11720 {
 	public static void main(String args[]) throws IOException {
@@ -12,7 +11,7 @@ public class boj_11720 {
 		int sum = 0;
 		
 		for(byte val : br.readLine().getBytes()) {
-			sum += (val - '0');
+			sum += (val - '0'); //utf 인코딩 맞게 변환. 
 		}
 		
 		System.out.println(sum);
--- IO/src/boj_11721.java
+++ IO/src/boj_11721.java
@@ -1,4 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class boj_11721 {
-
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int len = s.length()/10;
+		
+		for(int i =0; i<len; i++) {
+			System.out.println(s.substring(i*10, i*10+10));
+		}
+		System.out.println(s.substring(len*10));
+	}
 }
