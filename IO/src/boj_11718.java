93f9d7e87c082aaadee1c5fbfe1152005e3e83b8
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-12-12 19:00:34 +0900

    boj_11718 답변 수정

--- IO/src/boj_11718.java
+++ IO/src/boj_11718.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
 public class boj_11718 {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		
-		
-		while(sc.hasNext()) {
-			String s = sc.next();
-			System.out.println(s);
-		}
-	}
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNextLine()) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+    }
 }
