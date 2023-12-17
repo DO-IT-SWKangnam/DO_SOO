ad3b9ffc7443d78a86b2b095c9c2e00ab4c8fcdf
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-12-17 20:05:35 +0900

    Solved-11727

--- DP/src/boj_11727.java
+++ DP/src/boj_11727.java
@@ -15,7 +15,7 @@ public class boj_11727 {
 		dp[2] = 3;
 		
 		for(int i =3; i<=n; i++) {
-			dp[i] = (dp[i-1] + 2*dp[i-2]) & 10007;
+			dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007;
 		}
 		
 		System.out.println(dp[n]);
